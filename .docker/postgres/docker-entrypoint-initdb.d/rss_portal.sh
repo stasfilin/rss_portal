9c1fda3fdb6263859585d4f2fdfaadde61f2fe0f
Stanislav Filin <stasfilinmusic@gmail.com>
2020-05-22 22:34:15 +0300

    feat(docker): add docker
    Signed-off-by: Stanislav Filin <stasfilinmusic@gmail.com>

---
+++ .docker/postgres/docker-entrypoint-initdb.d/rss_portal.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
+psql -U postgres -c "ALTER USER $DB_USER CREATEDB"
+psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
