dcdcfd175c0f134307e9da1af41ff9d1e35468de
Stanislav Filin <stasfilinmusic@gmail.com>
2020-05-24 17:33:28 +0300

    feat(frontend): update comment service
    Signed-off-by: Stanislav Filin <stasfilinmusic@gmail.com>

--- frontend/src/services/comment.service.js
+++ frontend/src/services/comment.service.js
@@ -6,7 +6,7 @@ const API_URL = 'http://127.0.0.1:8000/api/';
 class CommentService {
   addComment(article, content) {
     return axios.post(API_URL + 'comment/', {
-        feed_item_id: article.id,
+        feed_item: article.id,
         content: content
       },
         { headers: authHeader() });
