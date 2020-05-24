c75a4c5d96308ca6113c7658ec4f06165daa7278
Stanislav Filin <stasfilinmusic@gmail.com>
2020-05-24 19:42:02 +0300

    feat(backend): change update feed function
    Signed-off-by: Stanislav Filin <stasfilinmusic@gmail.com>

--- backend/api/views/feed.py
+++ backend/api/views/feed.py
@@ -1,3 +1,5 @@
+import logging
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, mixins
 from rest_framework.decorators import action
@@ -10,6 +12,8 @@ from feed.exceptions import ParsingError
 from feed.filters import FeedItemFilterSet
 from feed.models import Feed, FeedItem
 from feed.parser import feed_parser
+
+logger = logging.getLogger(__name__)
 
 
 class FeedView(ModelViewSet):
@@ -40,7 +44,7 @@ class FeedView(ModelViewSet):
         """
         Add user when he update own feed
         """
-        serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user, attempt=0, terminated=False)
 
     @action(detail=True, methods=["get"])
     def fetch(self, request, pk=None) -> Response:
@@ -55,10 +59,31 @@ class FeedView(ModelViewSet):
             feed_parser(feed)
             return Response({"status": True}, status=status.HTTP_200_OK)
         except ParsingError as e:
+            logger.exception(e)
             return Response(
                 {"status": False, "message": str(e)},
                 status=status.HTTP_503_SERVICE_UNAVAILABLE,
             )
+
+    @action(detail=False, methods=["get"])
+    def fetch_all(self, request) -> Response:
+        """
+        Action for fetch feed manually
+        :param request: Django Request
+        :param pk: Feed ID
+        :return: Response with statuses 200 if valid and 503 if not
+        """
+        feeds = self.get_queryset()
+        total = 0
+        for feed in feeds:
+            try:
+                feed_parser(feed)
+                total += 1
+            except ParsingError as e:
+                logger.exception(e)
+        return Response(
+            {"total": feeds.count(), "finished": total}, status=status.HTTP_200_OK
+        )
 
 
 class FeedItemView(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericViewSet):
